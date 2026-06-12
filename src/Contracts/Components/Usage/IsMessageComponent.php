38b09d6e2873f970e9f884e230601a7015fbafd1
Justin K. <justin.k@justplayer.de>
2026-06-13 00:50:53 +0200

    Component Usage interfaces for context

---
+++ src/Contracts/Components/Usage/IsMessageComponent.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustinKluever\DiscordWebhookBuilder\Contracts\Components\Usage;
+
+/**
+ * Components that implement this interface can be used within messages
+ *
+ * @see https://docs.discord.com/developers/components/using-message-components
+ */
+interface IsMessageComponent {}
---
+++ src/Contracts/Components/Usage/IsModalComponent.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustinKluever\DiscordWebhookBuilder\Contracts\Components\Usage;
+
+/**
+ * Components that implement this interface can be used within modals
+ *
+ * @see https://docs.discord.com/developers/components/using-modal-components
+ */
+interface IsModalComponent {}
