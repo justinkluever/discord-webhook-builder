a8dc06c22aeafbab1703d4ff5a2eadd5a3b8031c
Justin K. <justin.k@justplayer.de>
2026-06-13 00:48:22 +0200

    IsComponentsV2 interface for context

---
+++ src/Contracts/Components/IsComponentsV2.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustinKluever\DiscordWebhookBuilder\Contracts\Components;
+
+/**
+ * Components that implement this interface require the IS_COMPONENTS_V2 message flag.
+ *
+ * @see https://docs.discord.com/developers/components/reference
+ */
+interface IsComponentsV2 {}
